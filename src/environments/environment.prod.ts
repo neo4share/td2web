f97e40f53db0a4a56c722c1014e16d5b803dd091
Pascal Groß <pascal.gross@external.telekom.de>
2024-03-07 16:25:58 +0100

    gh pages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://localhost:3000', // Beispiel-URL für die Produktionsumgebung
+    apiUrl: 'http://82.165.127.6:3000', // Beispiel-URL für die Produktionsumgebung
   };
   
